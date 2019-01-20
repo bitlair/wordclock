08ea3457d8e34469d737035b8c75b1dfd10ec8e0
Wilco Baan Hofman <synnack@users.noreply.github.com>
2019-01-20 23:08:14 +0100

    Fix language errors.

--- html/src/components/WifiWarning.vue
+++ html/src/components/WifiWarning.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="alert alert-warning" role="alert" id="wifialert" v-if="wifi.status !== 'connected'">
-    Let op! Op dit moment is de woord klok niet verbonden met een WiFi netwerk. Dit betekend dat de juiste tijd 
+    Let op! Op dit moment is de woordklok niet verbonden met een WiFi netwerk. Dit betekent dat de juiste tijd 
     niet opgehaald kan worden. Hierdoor kunnen ook functies als het instellen van kleuren niet goed werken. Ga
     naar de pagina <router-link to="/wifi">WiFi instellingen</router-link> om verbinding te maken.
   </div>
