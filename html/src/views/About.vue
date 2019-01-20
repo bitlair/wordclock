34710282774acf472dc2d2bc7685038b1f902bc5
Wilco Baan Hofman <synnack@users.noreply.github.com>
2019-01-20 23:03:10 +0100

    Fix language errors

--- html/src/views/About.vue
+++ html/src/views/About.vue
@@ -2,17 +2,17 @@
   <div class="about">
     <h1>About</h1>
     <p>
-    Deze woordklok is bij Hackerspace bitlair gebouwd.
-    voor meer informatie over bitlair (eerst met wifi netwerk verbinden) <a href="https://github.com/bitlair">Bitlair</a></p>
+    Deze woordklok is bij Hackerspace Bitlair gebouwd.
+    Voor meer informatie over Bitlair (eerst met wifi netwerk verbinden) <a href="https://github.com/bitlair">Bitlair</a></p>
 
     <p>        
     Meer over de klok zelf: <a href="https://github.com/bitlair/wordclock">https://github.com/bitlair/wordclock</a>
     </p>
 
     <h1>Credits</h1>
-    Deze klok is een uitbreiding op de vorige bitlair wordklok door voormalig Bitlair member Kartoffel (Niek):        
-    <a href="https://github.com/niekproductions/word-clock"> Niekproductions</a><br />
-    Dit was een uitbreiding van <a href="https://bitbucket.org/sjoerdtimmer/ledclock">de Randomdata klok</a><br />
-    Wat weer een uitbreiding was van <a href="https://bitbucket.org/vdham/wordclock.git"> Sjoerd</a>
+    Deze klok is een uitbreiding op de vorige Bitlair wordklok door voormalig Bitlair member Kartoffel (Niek):        
+    <a href="https://github.com/niekproductions/word-clock">Niekproductions</a>.<br />
+    Dit was een uitbreiding van <a href="https://bitbucket.org/sjoerdtimmer/ledclock">de Randomdata klok</a>,<br />
+    wat weer een uitbreiding was van <a href="https://bitbucket.org/vdham/wordclock.git"> Sjoerd</a>
   </div>
 </template>
